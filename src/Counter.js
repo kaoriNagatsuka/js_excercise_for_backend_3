9c71f99edda4863ae3a9a59e57bb319f7155c1f2
長塚かおり <nagatsukakaori@nagatsukakaorinoMacBook-Air.local>
2019-04-15 23:21:53 +0900

    Initial commit

---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+class Counter {
+    constructor() {
+        this._count = 0;
+    }
+    increment() {
+        this._count++;
+    }
+    decrement() {
+        this._count--;
+    }
+}
+module.exports = Counter;
